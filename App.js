1521e3deac3366f15d92fc2b564123468dbf5a60
riuteshyaaa <riteshyaaa@gmail.com>
2024-12-20 13:30:20 +0530

    created component

--- App.js
+++ App.js
@@ -1,24 +1,45 @@
 
 import React from 'react';
 import ReactDOM from "react-dom/client";
-const heading1=  React.createElement('h1', {
-    id: 'title',
-    key: 'title'
-
-}, 'Hello React World');
-
-const heading2=  React.createElement('h1', {
-    id: 'title',
-    key: 'key'
 
 
-}, 'Another way to create React element ');
+// const heading1=  React.createElement('h1', {
+//     id: 'title',
+//     key: 'title'
 
-const container=  React.createElement('div', {
-    id: 'container',
-    key: 'class'
+// }, 'Hello React World');
 
-}, [heading1, heading2]);
+// const heading2=  React.createElement('h1', {
+//     id: 'title',
+//     key: 'key'
+
+
+// }, 'Another way to create React element ');
+
+// const container=  React.createElement('div', {
+//     id: 'container',
+//     key: 'class'
+
+// }, [heading1, heading2]);
+
+
+
+const Title =  () =>( <h1>
+Hello React World
+</h1>)
+
+const Header = () => 
+     (
+        
+    <div>
+     <Title/>   
+        <h1>Rendering by component </h1>
+        <h2>Rendering by component </h2>
+       
+    </div>
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container)
+
+root.render(<Header />);
